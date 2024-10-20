e9cd9dcb9f6cbdd0b36a9ca6b959d335f2b01ff3
Parth Nautiyal <parthnautiyal1016@gmail.com>
2024-10-20 13:46:06 +0530

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   views/edit.ejs 	modified:   views/show.ejs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Campground = require('./models/campground')
 const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
+const methodOverride=require('method-override')
+
+
 
 main().catch(err => console.log(err));
 async function main(){
@@ -11,12 +14,12 @@ async function main(){
     console.log("Connected with Mongoose")
 }
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
 app.set('views',path.join(__dirname,'/views'))
 app.set('views engine','ejs')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req,res)=>{
@@ -34,10 +37,24 @@ app.get('/campgrounds/:id',async (req,res)=>{
     const camp = await Campground.findById(req.params.id);
     res.render('show.ejs',{ camp })
 })
+app.get('/campgrounds/:id/edit', async(req,res) =>{
+    const camp = await Campground.findById(req.params.id)
+    res.render('edit.ejs', { camp })
+})
+app.patch('/campgrounds/:id', async(req,res) =>{
+    const {id} = req.params;
+    const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${camp._id}`)
+})
 app.post('/campgrounds',async(req,res)=>{
     const camp = new Campground(req.body.campground)
     await camp.save()
     res.redirect(`/campgrounds/${camp._id}`)
+})
+app.delete('/campgrounds/:id', async(req,res) =>{
+    const {id} = req.params
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 app.listen(3000, ()=>{
     console.log("App runs at 3000!!!")
